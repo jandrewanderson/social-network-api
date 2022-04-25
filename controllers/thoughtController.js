e7531116c2f4c9ee00e3ba84b09e470bacfe11c1
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-04-25 15:23:36 -0500

    created routes and started controller files

---
+++ controllers/thoughtController.js
@@ -0,0 +1,34 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+const getThoughts = () => {
+
+}
+const getThoughtById = () => {
+
+}
+const createThought = () => {
+
+}
+const updateThought = () => {
+
+}
+const deleteThought = () => {
+
+}
+const createReaction = () => {
+
+}
+const deleteReaction = () => {
+
+}
+
+module.exports = {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+}
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+const getUsers = () => {
+
+}
+const getUserById = () => {
+
+}
+const createUser = () => {
+
+}
+const updateUser = () => {
+
+}
+const deleteUser = () => {
+
+}
+const addFriend = () => {
+
+}
+const deleteFriend = () => {
+
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+}
